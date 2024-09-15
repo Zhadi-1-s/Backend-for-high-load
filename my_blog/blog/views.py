dcab013faa1e1bdc783362988cd100289112dc47
Zhadi <zhadi000s@gmail.com>
2024-09-15 23:19:19 +0500

    Почти закончил первый асайнмент

---
+++ my_blog/blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from .models import Post
+
+from django.shortcuts import get_object_or_404
+
+from django.shortcuts import redirect
+
+from .forms import PostForm
+
+def blog_home(request):
+    return HttpResponse("Hello, Blog!")
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
